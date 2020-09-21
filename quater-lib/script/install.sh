ae19bb0496d59d70a1a166286092a3e863d0e44c
tbottini <thomasbottini@protonmail.com>
2020-09-21 19:07:35 +0200

    build: provide script for quick installation

---
+++ quater-lib/script/install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+libFolderName="lib.tmp"
+libPath="./"$libFolderName"/LibVueComp/quater-lib"
+
+mkdir $libFolderName
+git clone --single-branch --branch develop https://github.com/tbottini/LibVueComp.git ./$libFolderName/LibVueComp
+
+cd $libPath
+yarn lib
+
+cd ../../../
+yarn add $libPath
+
+rm -rf lib.tmp
