ed42c0af9609652e728e55d111445a14d10916c8
tbottini <thomasbottini@protonmail.com>
2020-09-21 21:36:16 +0200

    feat(comp): move basic imagelist énumerator... not finish

---
+++ quater-lib/src/components/imageList.vue
@@ -0,0 +1,33 @@
+<template>
+    <ul>
+        <li v-for="(item, index) in items" :key="index">
+            {{ item[printKey] }}
+            <slot v-bind:item="item"></slot>
+        </li>
+    
+    </ul>
+</template>
+<script>
+
+export default {
+    name: "ImageList",
+    props: {
+        "items": {
+            type: Array,
+            required: true
+        },
+        "printKey": {
+            type: String,
+            default: "name"
+        }
+    },
+}
+
+</script>
+<style lang="sass" scoped>
+@import '@/styles/variables.sass'
+
+li
+    color: $primary
+
+</style>
