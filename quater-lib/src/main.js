5180efe958f9aa481f63e1f07cdaeebb74b2a667
tbottini <thomasbottini@protonmail.com>
2020-09-22 18:19:48 +0200

    build: export auto all comp in folder components

--- quater-lib/src/main.js
+++ quater-lib/src/main.js
@@ -1,3 +1,30 @@
-import DummyButton from './components/DummyButton.vue'
 
-export default DummyButton
+import upperFirst from 'lodash/upperFirst'
+import camelCase from 'lodash/camelCase'
+
+const components = require.context(
+    './components',
+    false,
+    /[A-Z]\w+\.(vue|js)$/
+  );
+
+export default {
+  install (Vue) {
+    components.keys().forEach(component =>
+    {
+        const componentElement = components(component);
+        const componentName = upperFirst(
+            camelCase(
+                component
+                .split('/')
+                .pop()
+                .replace(/\.\w+$/, '')
+            ))
+
+        Vue.component(
+            componentName,
+            componentElement.default
+        );
+    })
+  }
+}
