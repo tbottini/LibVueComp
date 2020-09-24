2575d8fb11bec8fc142799c3e88d3e4cf143c983
tbottini <thomasbottini@protonmail.com>
2020-09-24 16:05:47 +0200

    feat(comp): add basic input and searchInput

---
+++ quater-lib/src/components/InputString.vue
@@ -0,0 +1,23 @@
+<template>
+    <input type="text" :placeholder="placeholder" /> 
+</template>
+<script>
+export default {
+    name: "InputString",
+    props: {
+        placeholder: {
+            type: String,
+            required: false,
+        }
+    }
+}
+</script>
+<style lang="sass" scoped>
+
+input
+    outline: none
+    border: none
+    border-radius: 5px
+    font-size: 1em
+
+</style>
---
+++ quater-lib/src/components/SearchBar.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <InputString
+            placeholder="find movie..."
+        />
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped lang="sass">
+
+input
+    border: none
+    width: 100%
+    height: 100%
+    padding: 10px
+    border-radius: 5px
+    background-color: white
+    color: black
+    font-size: 1em
+    border-bottom: 1px solid white
+    box-shadow: 0px 1px 3px 0px lightgrey
+
+div
+    width: 100%
+
+</style>
