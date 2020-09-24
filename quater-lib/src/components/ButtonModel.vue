3a91d60f79a6da27274cd2347b8e14c9aba7550e
tbottini <thomasbottini@protonmail.com>
2020-09-24 17:15:39 +0200

    feat(button): implement basic button with style

---
+++ quater-lib/src/components/ButtonModel.vue
@@ -0,0 +1,30 @@
+<template>
+    <button>
+        <slot>
+            button
+        </slot>
+    </button>
+</template>
+<script>
+export default {
+    name: "ToggleButton",
+}
+</script>
+<style lang="sass" scoped>
+@import '@/styles/colors.sass'
+
+
+button
+    background-color: $purpleDark
+    border-radius: 7px
+    border: 2px solid white
+    color: white
+    height: 40px
+    min-width: 30px
+    transition: 0.1s
+    
+
+button:hover
+    box-shadow: 0px 1px 3px 0px  white
+
+</style>
