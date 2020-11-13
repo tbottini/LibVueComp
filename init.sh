1c2af7bb84ab697f59890495aebf53b6d2347fc9
tbottini <thomasbottini@protonmail.com>
2020-11-13 20:28:11 +0100

    fix: bad balise package.json

--- init.sh
+++ init.sh
@@ -12,8 +12,8 @@ git clone https://github.com/tbottini/libvuecomp --branch nuxt-lib
 mv libvuecomp/ressources/* .
 rm -rf libvuecomp
 
-sed -i -e 's/<name>/'$name'/g' www/package.json www/nuxt.config.js www/api/index.js www/api/init.js
-sed -i -e 's/<admin-mail>/'$name'@factice.com/g' www/api/init.js
+sed -i -e 's/<name>/'"$name"'/g' www/package.json www/nuxt.config.js www/api/index.js www/api/init.js
+sed -i -e 's/<admin-mail>/'"$name"'@factice.com/g' www/api/init.js
 
 cd www && yarn 
 
