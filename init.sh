2bf60a38f7bd657da063fbbb7ca593ede81027cf
tbottini <thomasbottini@protonmail.com>
2020-11-13 19:50:57 +0100

    fix(init): change path of ressources update

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 #ask name project
 
-echo -ne "project name : "
+echo -n "project name : "
 read name
 
 
@@ -12,8 +12,7 @@ git clone https://github.com/tbottini/libvuecomp --branch nuxt-lib
 mv libvuecomp/ressources/* .
 rm -rf libvuecomp
 
-cd www
-sed -i -e 's/<name>/'$name'/g' package.json nuxt.config.json index.js init.js
-sed -i -e 's/<admin-name>/'$name'/g' init.js
+sed -i -e 's/<name>/'$name'/g' www/package.json www/nuxt.config.js www/api/index.js www/api/init.js
+sed -i -e 's/<admin-mail>/'$name'@factice.com/g' www/api/init.js
 
 #choose option for database  sqlite
